8a0b7fe496e868bc6bde1a9799877d453133e625
Berkan Yıldırım <berkanyldrim@gmail.com>
2022-03-06 12:35:21 +0300

    week4: Page content added. Topwrapper and sidebar style changed.

---
+++ src/components/PageContent.vue
@@ -0,0 +1,59 @@
+<template>
+<!-- Page Content Start -->
+    <div class="berkan">
+
+        <div class="main-text"><p>Welcome to my VueJs Homework</p>
+        
+        <button class="btn btn-go"> <a class="link-go" href="https://github.com/171-Logo-Siber-FullStack-Bootcamp/week4-assignment-berkanyldrim">Go to Project Repository</a> </button>
+        
+        </div>
+        
+
+    </div>
+<!-- Page Content END -->
+</template>
+<script>
+    export default{
+        name:"PageContent"
+    }
+</script>
+<style>
+.berkan{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url(../../public/img/nature.jpg);
+    background-position: center;
+    background-size: cover;
+    height: 100%;
+}
+.main-text{
+    margin-top: -100px;
+    display: block;
+    
+    font-size: 72px;
+    font-weight: 600;
+    color: white !important;
+}
+.btn-go{
+    margin-left: 40%;
+   border: solid 1px #505050;
+    
+    background-color: #203239;
+    height: 60px;
+    
+    border-radius: 10px !important;
+    
+}
+.link-go{
+    
+    text-decoration: none !important;
+    color: white;
+    font-weight: bold;
+
+}
+.link-go:hover{
+    color: white;
+    font-weight: bold;
+}
+</style>
--- src/components/WrapperTop.vue
+++ src/components/WrapperTop.vue
@@ -1,15 +1,23 @@
 <template>
  <!-- Content Wrapper START -->
     <div id="content-wrapper" class="d-flex flex-column">
+        <!-- Main Content START -->
         <MainContent></MainContent>
+        <!-- Main Content END -->
+
+
+        <!-- Page Content START -->
+        <PageContent></PageContent>
+        <!-- Page Content END -->
     </div>
  <!-- Content Wrapper END -->
 </template>
 
 <script>
 import MainContent from "./fullWrapper/MainContent.vue";
+import PageContent from "./PageContent.vue";
 export default{
     name: "WrapperTop",
-    components: { MainContent }
+    components: { MainContent, PageContent }
 }
 </script>
